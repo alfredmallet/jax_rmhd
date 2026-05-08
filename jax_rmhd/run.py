8a8ec9174bfc400465dc5848cbe78ee99358a9f9
Alfred Mallet <alfred.mallet@berkeley.edu>
2026-05-08 11:12:13 -0700

    Changed logic of simulate loop from block_of_steps to a jax.lax.while_loop, since it doesnt make a significant difference to performance. This allows better control of snapshot times.

--- jax_rmhd/run.py
+++ jax_rmhd/run.py
@@ -1,9 +1,15 @@
-from .timestepping import *
-from .snapshot_io import *
+import jax
+from jax import jit
+import jax.numpy as jnp
+from .timestepping import block_of_steps, rk_advance
+from .snapshot_io import save_snapshot
 
-def simulate(initial_state,kgrid,params,nblock,t_snap,t_end,mngr):
-    #currently an orbax checkpoint mngr must be set outside of the simulate function
-    #this makes it a little easier to set up snapshots etc but could be changed
+#currently an orbax checkpoint mngr must be set outside of the simulate function
+#this makes it a little easier to set up snapshots etc but could be changed
+
+def simulate_scan(initial_state,kgrid,params,nblock,t_snap,t_end,mngr):
+    # this simulates for a fixed number of timesteps
+    # for automatic differentiation sometime in the future
     state=initial_state
     t_last_snapshot = state.t
     snap=0
@@ -22,3 +28,24 @@ def simulate(initial_state,kgrid,params,nblock,t_snap,t_end,mngr):
     save_snapshot(snap,state,mngr)
     mngr.wait_until_finished()
     return f"Ending simulation at t = " + str(state.t)
+
+def simulate(initial_state,kgrid,params,t_snap,t_end,mngr):
+    def stepping(state):
+        return rk_advance(state,kgrid,params)
+    state=initial_state
+    t_last_snapshot = state.t
+    snap=0   
+    print("Saving initial state as snapshot "+str(snap))
+    save_snapshot(snap,state,mngr)
+    while state.t<t_end:
+        snap=snap+1
+        def snap_cond(state):
+            t_next_snapshot=t_last_snapshot+t_snap
+            return state.t<t_next_snapshot
+        state = jax.lax.while_loop(snap_cond,stepping,state)
+        print ("Saving snapshot "+str(snap)+ " at t = "+str(state.t))
+        save_snapshot(snap,state,mngr)
+        t_last_snapshot=state.t
+    mngr.wait_until_finished()
+    return f"Ending simulation at t = "+str(state.t)
+
